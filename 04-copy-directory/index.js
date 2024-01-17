837b40b394a9f505d76281c421841cfaaaec1467
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2024-01-18 01:02:56 +0300

    feat: add an opportunity to copy folders with their content inside

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,15 +4,23 @@ const path = require('path');
 const originFolderPath = path.join(__dirname, 'files');
 const copyFolderPath = path.join(__dirname, 'files-copy');
 
-const readOriginalFolder = async () => {
+const readOriginalFolder = async (currentFolder, targetFolder) => {
   try {
-    const arr = await fsPromises.readdir(originFolderPath, { recursive: true });
-    arr.forEach((file) => {
-      fsPromises.copyFile(
-        path.join(originFolderPath, file),
-        path.join(copyFolderPath, file),
-      );
-    });
+    const arr = await fsPromises.readdir(currentFolder, { recursive: true });
+
+    for (const file of arr) {
+      const sourceFilePath = path.join(currentFolder, file);
+      const targetFilePath = path.join(targetFolder, file);
+
+      const stat = await fsPromises.stat(sourceFilePath);
+
+      if (stat.isFile()) {
+        await fsPromises.copyFile(sourceFilePath, targetFilePath);
+      } else {
+        await fsPromises.mkdir(targetFilePath, { recursive: true });
+        await readOriginalFolder(sourceFilePath, targetFilePath);
+      }
+    }
   } catch (err) {
     console.error(err);
   }
@@ -22,7 +30,8 @@ const createCopyFolder = async () => {
   try {
     await fsPromises.rm(copyFolderPath, { recursive: true, force: true });
     await fsPromises.mkdir(copyFolderPath, { recursive: true });
-    await readOriginalFolder();
+    await readOriginalFolder(originFolderPath, copyFolderPath);
+
     console.log('Folder copied successfully');
   } catch (err) {
     console.error(err);
