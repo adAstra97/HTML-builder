989ffed79616d8a7cb75ae7d0758c79c0e86b608
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2024-01-17 15:06:02 +0300

    fix: add variable extname

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,10 +6,13 @@ fsPromises.readdir(secretFolderPath, { withFileTypes: true }).then((res) => {
   res.forEach((item) => {
     if (item.isFile()) {
       const filePath = path.join(secretFolderPath, item.name);
-      const convertedPath = item.name.replace('.', ' - ');
+      const extname = path.extname(filePath);
+      const basename = path.basename(filePath, extname);
 
       fsPromises.stat(filePath).then((stat) => {
-        console.log(`${convertedPath} - ${(stat.size / 1024).toFixed(3)}kb`);
+        console.log(
+          `${basename} - ${extname.slice(1)} - ${(stat.size / 1024).toFixed(3)}kb`,
+        );
       });
     }
   });
